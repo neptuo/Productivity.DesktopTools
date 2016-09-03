543d8a8bd04946b2c58cdd2690d3848a8be1019d
Marek Fišera <mara@neptuo.com>
2016-09-03 09:06:56 +0000

    Preparing for TopMost windows.

---
+++ src/WinRun.UI/TopMostWindows/PinTopMostHotkeyHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WinRun.Hotkeys;
+
+namespace WinRun.TopMostWindows
+{
+    public class PinTopMostHotkeyHandler : IHotkeyHandler
+    {
+        public Hotkey Hotkey
+        {
+            get { return new Hotkey(ModifierKeys.Windows | ModifierKeys.Shift, Key.T); }
+        }
+
+        public void Handle(Hotkey hotkey)
+        {
+            IntPtr activeWindowHandle = Win32.GetForegroundWindow();
+            // TODO: SetWindowPos with Win32.TopMost;
+        }
+    }
+}
---
+++ src/WinRun.UI/TopMostWindows/Win32.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinRun.TopMostWindows
+{
+    internal static class Win32
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        public static IntPtr TopMost = new IntPtr(-1);
+        public static IntPtr NoTopMost = new IntPtr(-2);
+    }
+}
